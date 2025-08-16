09ed22d5804358fe81908ec88bc7a76cc9fbafb5
Tano Christian <tanochristian84@gmail.com>
2025-08-16 01:03:20 +0000

    Update script.js
    The implementation of the whole app.

--- Javascript simple app/script.js
+++ Javascript simple app/script.js
@@ -1,1 +1,25 @@
-alert("Test")
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addtask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "li"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false)
